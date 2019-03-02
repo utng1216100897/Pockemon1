f4239fdbbb4ac4fb867113e8b967f389ba7d8185
LABMAC13 <LABMAC13@iMac-de-LABMAC13.local>
2019-03-01 20:08:27 -0600

    Initial Commit

---
+++ PockemonV/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PockemonV
+//
+//  Created by LABMAC13 on 01/03/19.
+//  Copyright © 2019 LABMAC13. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
